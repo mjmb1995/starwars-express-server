3a0379441787315efe360ee44a568e010fdb787e
Mark Joshua Bihag <markjoshuabihag@gmail.com>
2017-11-18 07:53:30 -0800

    added html

--- server1.js
+++ server1.js
@@ -1,67 +1,92 @@
 // Dependencies
-// ===========================================================
+// =============================================================
 var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 
+// Sets up the Express App
+// =============================================================
 var app = express();
 var PORT = 3000;
 
-// Data
-// ===========================================================
-var yoda = {
-  name: "Yoda",
-  role: "Jedi Master",
-  age: 900,
-  forcePoints: 2000
-};
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-var darthmaul = {
-  name: "Darth Maul",
-  role: "Sith Lord",
-  age: 200,
-  forcePoints: 1200
-};
-
-// Create one more data entry for the character Obi Wan Kenobi.
-// Enter any values you like for the parameters following the same format as the Yoda and Darth Maul character
-//
-
-// YOUR CODE GOES HERE
-var obiwankenobi = {
-  name: "Obi Wan Kenobi",
-  role: "Jedi",
-  age: 60,
-  forcePoints: 500
-};
-//
+// Star Wars Characters (DATA)
+// =============================================================
+var characters = [
+  {
+    routeName: "yoda",
+    name: "Yoda",
+    role: "Jedi Master",
+    age: 900,
+    forcePoints: 2000
+  },
+  {
+    routeName: "darthmaul",
+    name: "Darth Maul",
+    role: "Sith Lord",
+    age: 200,
+    forcePoints: 1200
+  },
+  {
+    routeName: "obiwankenobi",
+    name: "Obi Wan Kenobi",
+    role: "Jedi Master",
+    age: 55,
+    forcePoints: 1350
+  }
+];
 
 // Routes
-// ===========================================================
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.send("Welcome to the Star Wars Page!");
+  // res.send("Welcome to the Star Wars Page!")
+  res.sendFile(path.join(__dirname, "view.html"));
 });
 
-app.get("/yoda", function(req, res) {
-  res.json(yoda);
+// Get all characters
+app.get("/all", function(req, res) {
+  res.json(characters);
 });
 
-app.get("/darthmaul", function(req, res) {
-  res.json(darthmaul);
+// Search for Specific Character (or all characters) - provides JSON
+app.get("/api/:characters?", function(req, res) {
+  var chosen = req.params.characters;
+
+  if (chosen) {
+    console.log(chosen);
+
+    for (var i = 0; i < characters.length; i++) {
+      if (chosen === characters[i].routeName) {
+        return res.json(characters[i]);
+      }
+    }
+    return res.json(false);
+  }
+  return res.json(characters);
 });
 
+// Create New Characters - takes in JSON input
+app.post("/api/new", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var newcharacter = req.body;
 
+  console.log(newcharacter);
 
-// Create a new Express route that leads users to the new Obi Wan Kenobi Data
-// Follow the same format as the Yoda and Darth Maul routes
-//
+  // We then add the json the user sent to the character array
+  characters.push(newcharacter);
 
-// YOUR CODE GOES HERE
-//
-//
-app.get("/obiwankenobi", function(req, res) {
-  res.json(obiwankenobi);
+  // We then display the JSON to the users
+  res.json(newcharacter);
 });
-// Listener
-// ===========================================================
+
+// Starts the server to begin listening
+// =============================================================
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
